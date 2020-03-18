193618159b41d48d7a8c031997e1a8cd05dde778
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-18 18:24:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,9 @@ class Triangle
   
   def kind 
     if valid?
-      if @triangle_sides.uniq.length == 1
+      if @sides.uniq.length == 1
         return :equilateral
-      elsif @triangle_sides.uniq.length == 2
+      elsif @sides.uniq.length == 2
         return :isosceles
       else
         return :scalene
