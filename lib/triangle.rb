1fb644dcc1d025508c233a26c471bdbb64e9db1f
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-18 17:59:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   
   attr_accessor :equilateral, :isosceles, :scalene
   
-  def kind(:equilateral, :isosceles, :scalene)
+  def kind(equilateral, :isosceles, :scalene)
     
   end
 end
